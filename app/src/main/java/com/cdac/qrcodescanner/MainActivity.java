813eb951300f70a55ef1e1fa262fb4331c70a874
Suraj <vishwakarmasuraj678@gmail.com>
2022-04-18 18:37:32 +0530

    QRCode Scanner

---
+++ app/src/main/java/com/cdac/qrcodescanner/MainActivity.java
@@ -0,0 +1,33 @@
+package com.cdac.qrcodescanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents(){
+        findViewById(R.id.buttonTakePicture).setOnClickListener(this);
+        findViewById(R.id.buttonScanBarcode).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonScanBarcode:
+                startActivity(new Intent(this,ScannerBarcodeActivity.class));
+                break;
+            case R.id.buttonTakePicture:
+                startActivity(new Intent(this,PictureBarcodeActivity.class));
+                break;
+        }
+    }
+}
